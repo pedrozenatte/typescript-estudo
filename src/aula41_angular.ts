58673f37e63331be63acd8cab2a4e587c9f5ebc3
Pedro Zenatte <pedrozenatte@usp.br>
2026-02-04 23:48:31 -0300

    Uma breve explicação sobre angular e nestjs

---
+++ src/aula41_angular.ts
@@ -0,0 +1,6 @@
+// ANGULAR 
+
+/**
+ * O que é Angular? 
+ * Angular é um framework para aplicações frontend completo, escrito em TypeScript, que te obriga a seguir uma arquitetura bem definida para criar aplicações web grandes e organizadas.
+ */
---
+++ src/aula42_nestjs.ts
@@ -0,0 +1,24 @@
+// NESTJS 
+
+/**
+ * O que é o NestJS?
+ * NestJS é um framework para aplicações backend para Node.js, escrito em TypeScript, que impõe uma arquitetura organizada para construir APIs e sistemas escaláveis.
+ * 
+ * Ideia central do NestJS
+ * Você declara o que cada parte é. O Nest cria, conecta e executa tudo por você, ou seja você não:
+ * - instancia classes com new
+ * - registra rotas manualmente
+ * - gerencia ciclo de vida
+ * - O Nest faz isso usando:
+ *      - decorators
+ *      - injeção de dependência
+ *      - metadados
+ *      - runtime
+ * 
+ * Focado em arquitetura Modular e escalável
+ * Fornece um padrão arquitetural
+ * Os recursos do NestJS estão disponíveis através de decorators
+ * Injeção de dependência
+ * Utiliza o express para gerenciamento de rotas
+ * 
+ */
